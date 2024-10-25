e208502c61f5ec6e0c3ac0e69253bff55133626b
AndresS2100 <90879362+AndresS2100@users.noreply.github.com>
2024-10-24 21:07:20 -0600

    Finalizacion semana 06

--- src/main/java/com/tienda/dao/CategoriaDao.java
+++ src/main/java/com/tienda/dao/CategoriaDao.java
@@ -5,6 +5,6 @@ import com.tienda.domain.Categoria;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
-public interface CategoriaDao extends JpaRepository<Categoria, Long>{
+public interface CategoriaDao extends JpaRepository <Categoria, Long> {
     
 }
